0b77d8758fc1197e057f90a26ac47cebe1758fa3
Shershneva Kseniya <megacoolksuha@mail.ru>
2026-06-18 23:56:37 +0300

    chore: changes for deployment part3

--- user-panel/server/dist/utils/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BASE_URL_CLIENT = void 0;
-exports.BASE_URL_CLIENT = 'http://localhost:5173';
--- user-panel/server/dist/utils/getUniqIdValue.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUniqIdValue = void 0;
-const uuid_1 = require("uuid");
-const getUniqIdValue = () => {
-    return (0, uuid_1.v4)();
-};
-exports.getUniqIdValue = getUniqIdValue;
